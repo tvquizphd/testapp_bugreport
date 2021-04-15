87a4020e91c9904f06414d912e78fefd9b9174ac
TV Quiz PhD <tvquizphd@gmail.com>
2021-04-15 01:16:54 -0400

    just remove all functionality to test parcel works

--- packages/testapp/src/App.jsx
+++ packages/testapp/src/App.jsx
@@ -1,29 +1,11 @@
 // src/App.js
-import { graphql, QueryRenderer } from "react-relay";
-
 import environment from "./relay";
-
-const appQuery = graphql`
-    query AppQuery {
-        me {
-            data {
-                id
-                comments
-                created_at
-            }
-        }
-    }
-`;
 
 const App = () => {
     return (
-        <QueryRenderer
-            environment={environment}
-            query={appQuery}
-            render={({ error, props }) => {
-                console.log(props);
-            }}
-        />
+        <div>
+          {JSON.stringify({environment})}
+        </div>
     );
 };
 
